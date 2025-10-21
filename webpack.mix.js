fd3526e0586ef8e08d5dee0d8e0a638d053f9265
Jayson1215 <jayson.velasco@urios.edu.ph>
2025-10-21 14:43:40 +0800

    KARON PA HAPIT NA

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,6 +8,11 @@ mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/dashboard.scss', 'public/css')
    .sass('resources/sass/users.scss', 'public/css') // Users page styles
    .sass('resources/sass/students.scss', 'public/css') // Students page styles
+   .sass('resources/sass/faculty.scss', 'public/css') // Faculty page styles
+   .sass('resources/sass/departments.scss', 'public/css') // Departments page styles
+   .sass('resources/sass/courses.scss', 'public/css') // Courses page styles
+   .sass('resources/sass/academicyears.scss', 'public/css') // Academic Years page styles   
+   
    .options({
        processCssUrls: false,
    })
